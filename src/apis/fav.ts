4bbb76b82d711f585c547015e997e5d41b1edb1e
EnduIf <5352931+EnduIf@users.noreply.github.com>
2023-11-22 16:27:07 +0100

    .

---
+++ src/apis/fav.ts
@@ -0,0 +1,29 @@
+import { getCheerio } from '../utils/cheerio'
+import { getMainPage } from './getMainPage'
+import { Route } from './route'
+import type { Engine } from '../core/engine'
+import { Cheerio, Element } from 'cheerio'
+import { videoPage } from '../scrapers/pages/video'
+import { getToken } from './getToken'
+import { UrlParser } from '../utils/url'
+
+export interface FavouriteResult {
+    action: string
+    message: string
+    url: string
+    success: string
+  }
+  
+export async function favourite(engine: Engine, urlOrId: string): Promise<FavouriteResult> {
+    
+    const token = getToken(engine);
+    const videoID = UrlParser.getVideoID(urlOrId)
+
+    const res = await engine.request.post(Route.favourite(), {
+        token,
+        toggle: 1,
+        id: videoID,
+    })
+    const result = await res.json() as FavouriteResult
+    return result
+}
